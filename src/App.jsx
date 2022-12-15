ce42dd485971fefa6629cab1bf186211d6c5a016
ismael <ismaelwadejr07@gmail.com>
2022-12-15 11:40:14 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BasicExample from './components/menu'
 import Footer from './footer'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <BasicExample />
       <Footer />
     </div>
   )
